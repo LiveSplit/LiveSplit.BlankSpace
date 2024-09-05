a47d7a4c50a542ae61241ad3c37af8b5ba346bbf
Ero <56401411+just-ero@users.noreply.github.com>
2024-09-05 03:42:00 +0200

    Add `.editorconfig` & apply style rules (#6)
    * Add `.editorconfig`
    
    * Upgrade to C# 12
    
    * Use file-scoped namespaces, move `using` directives, fix whitespace
    
    * `IDE0005`, `IDE0065`, `IDE0161`
    
    * Add and order modifiers
    
    * `IDE0036`, `IDE0040`, `IDE0044`, `IDE0064`, `IDE0250`, `IDE0251`
    
    * Apply member body preferences
    
    * `IDE0021`, `IDE0022`, `IDE0023`, `IDE0024`, `IDE0025`, `IDE0026`, `IDE0027`, `IDE0053`, `IDE0061`
    
    * Apply braces, parentheses, new-line preferences
    
    * `IDE0011`, `IDE0047`, `IDE0048`, `IDE2000`, `IDE2001`, `IDE2002`, `IDE2003`, `IDE2004`, `IDE2005`, `IDE2006`
    
    * Apply `var` preferences, simplify `new` expression
    
    * `IDE0007`, `IDE0008`, `IDE0090`
    
    * Use index/range operator
    
    * `IDE0056`, `IDE0057`
    
    * Enforce code style in build

--- src/LiveSplit.BlankSpace/UI/Components/BlankSpaceFactory.cs
+++ src/LiveSplit.BlankSpace/UI/Components/BlankSpaceFactory.cs
@@ -1,27 +1,30 @@
-﻿using LiveSplit.Model;
+﻿using System;
+
+using LiveSplit.Model;
 using LiveSplit.UI.Components;
-using System;
 
 [assembly: ComponentFactory(typeof(BlankSpaceFactory))]
 
-namespace LiveSplit.UI.Components
+namespace LiveSplit.UI.Components;
+
+public class BlankSpaceFactory : IComponentFactory
 {
-    public class BlankSpaceFactory : IComponentFactory
+    public string ComponentName => "Blank Space";
+
+    public string Description => "Shows empty space with a configurable size.";
+
+    public ComponentCategory Category => ComponentCategory.Other;
+
+    public IComponent Create(LiveSplitState state)
     {
-        public string ComponentName => "Blank Space";
-
-        public string Description => "Shows empty space with a configurable size.";
-
-        public ComponentCategory Category => ComponentCategory.Other;
-
-        public IComponent Create(LiveSplitState state) => new BlankSpace();
-
-        public string UpdateName => ComponentName;
-
-        public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.BlankSpace.xml";
-
-        public string UpdateURL => "http://livesplit.org/update/";
-
-        public Version Version => Version.Parse("1.8.29");
+        return new BlankSpace();
     }
+
+    public string UpdateName => ComponentName;
+
+    public string XMLURL => "http://livesplit.org/update/Components/update.LiveSplit.BlankSpace.xml";
+
+    public string UpdateURL => "http://livesplit.org/update/";
+
+    public Version Version => Version.Parse("1.8.29");
 }
